4d2ba55c7f5ba498c098a447c00dc4b6c3b72280
sta-973 <is.setia1@gmail.com>
2025-10-16 09:36:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_templates, request, jsonify
 from flask_cors import CORS
 import subprocess
 import os
@@ -60,3 +60,4 @@ def get_file(filename):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
